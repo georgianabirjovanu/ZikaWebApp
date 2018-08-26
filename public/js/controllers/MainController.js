05d257527c573d24c4604ba3288ff8df6557655d
André Lopes <oandrevictor@gmail.com>
2018-08-26 11:52:56 -0300

    :hatched_chick: style fixes/changes login error message/ agents(mocked) list :hatched_chick:

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,8 +1,26 @@
-angular.module('zikaApp').controller('MainController', ['$scope','$location','$mdSidenav', '$cookies', '$rootScope', function($scope, $location, $mdSidenav, $cookies, $rootScope) {
+angular.module('zikaApp').controller('MainController', ['$scope','$location','$mdSidenav', '$cookies', '$rootScope', '$mdToast', function($scope, $location, $mdSidenav, $cookies, $rootScope, $mdToast) {
+
+  var toast_position = ['top', 'right'];
+
+  $scope.toast_position = toast_position.join(' ');
+
+  $scope.couldNotLoginToast = function() {
+    var pinTo = $scope.toast_position;
+
+    $mdToast.show(
+      $mdToast.simple()
+        .textContent('Não foi possível fazer login! E-mail e/ou senha incorretos')
+        .position(pinTo )
+        .hideDelay(3000)
+    );
+  };
+
 
   if (!$rootScope.user_info){
     $rootScope.user_info = $cookies.getObject('user');
   }
+
+  console.log($rootScope.user_info)
 
 
   $scope.taglinee = 'Nothing beats a pocket protector!';
@@ -24,6 +42,13 @@ angular.module('zikaApp').controller('MainController', ['$scope','$location','$m
      return !!$rootScope.user_info;
    }
 
+   $scope.gotoProperties = function(){
+     $location.path('properties');
+   }
+
+   $scope.gotoDashboard = function(){
+     $location.path('dashboard');
+   }
 
 
 
